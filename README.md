0118649d74111385d88ee55c1e9f70390658611d
Alexander Schwarze <shikami97@gmx.de>
2021-10-17 23:25:04 +0200

    Initial (new) project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Chess
 Learning Kotlin by programming a chess game
+
+### Information
+I really don't know, what I am doing. Code will definitely need a review
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,36 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
 group = "me.shika"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnit()
+    implementation(compose.desktop.currentOs)
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Chess"
+            packageVersion = "1.0.0"
+        }
+    }
 }
